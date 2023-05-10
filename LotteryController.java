717b63cce18fd22343e1c0d44949d6374335490d
2ono <clt3555@gmail.com>
2023-05-10 09:05:39 +0900

    MVC pattern added

--- LotteryController.java
+++ LotteryController.java
@@ -31,12 +31,6 @@ public class LotteryController {
 
 			model.setLuckyNum();
 			
-//			model.getLotteryNum();
-//			
-//			model.getLuckyNum();
-
-			// 당첨 번호 매칭
-//			model.setwinnerPickUp();
 			
 			for (int i = 0, j = 0; i < this.luckyNumber.length; i++) {
 				for (int x = 0; x < this.lotteryNum.length; x++) {
@@ -44,19 +38,19 @@ public class LotteryController {
 						i++;
 						System.out.printf("총 번호 %d번 당첨입니다\n", ++j);
 						if (j == 6) {
-							System.out.println("6개 일치 상금 : 2,000,000,000원 (20억)");
+							view.firstPrize();
 						}
 						else if (j == 5) {
-							System.out.println("5개 일치 + 보너스 볼 상금 : 30,000,000원");
+							view.seceonPrize();
 						}
 						else if (j == 4) {
-							System.out.println("4개 일치 시 상금 : 50,000원");
+							view.thirdPrize();
 						}
 						else if (j == 3) {
-							System.out.println("3개 일치 시 상금 : 5,000원");
+							view.fourthPrize();
 						}
 						else if (j >= 2) {
-							System.out.println("꽝입니다.");
+							view.lostMoney();
 						}
 						
 					}
--- LotteryView.java
+++ LotteryView.java
@@ -5,4 +5,24 @@ public class LotteryView {
 	public void checkPay() {
 		System.out.println("지불할 금액을 1000원 단위로 입력하세요.");
 	}
+	
+	public void firstPrize() {
+		System.out.println("6개 일치 상금 : 2,000,000,000원 (20억)");
+	}
+	
+	public void seceonPrize() {
+		System.out.println("5개 일치 + 보너스 볼 상금 : 30,000,000원");
+	}
+	
+	public void thirdPrize() {
+		System.out.println("4개 일치 시 상금 : 50,000원");
+	}
+	
+	public void fourthPrize() {
+		System.out.println("3개 일치 시 상금 : 5,000원");
+	}
+	
+	public void lostMoney() {
+		System.out.println("꽝입니다.");
+	}
 }
