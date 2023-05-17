e2fb04ba14866afda4cd6476be5906e55d137419
ReLuee <son7162@gmail.com>
2023-05-17 09:24:27 +0900

    [UPDATE] 230517

--- 손영석/lotto_simulator/LottoSimulator.java
+++ 손영석/lotto_simulator/LottoSimulator.java
@@ -3,11 +3,11 @@ package lotto_simulator;
 public class LottoSimulator {
 //	static LottoSimulatorModel model = new LottoSimulatorModel();
 //	static LottoSimulatorView view = new LottoSimulatorView();
-	static LottoSimulatorController control = new LottoSimulatorController();
+	static LottoSimulatorController controller = new LottoSimulatorController();
 	
 	public static void main(String[] args) {
 		
-		control.mainMenu();
+		controller.mainMenu();
 		
 	}
 }
